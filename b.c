da528a5dc415d26f47106abf5b318242804c0190
Lucas Gabriel <lucasbsb100@gmail.com>
2020-03-17 22:49:22 -0300

    ac

---
+++ b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	int unsigned n;
+	while(scanf("%d",&n) != EOF)
+	{
+		num++;
+	}
+
+	printf("%d\n", num);
+	printf("%d\n", EOF);
+
+
+	return 0;
+}
---
+++ c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int d, a,resp;	
+	scanf("%d %d",&d, &a);
+	resp = d-a;
+	printf("%d\n",resp);
+
+	return 0;
+}
+

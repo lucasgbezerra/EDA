f3f1de67d39c602ddf15ab0b404cacb6892dabf9
Lucas Gabriel <lucasbsb100@gmail.com>
2020-09-29 16:49:03 -0300

    melhorando o código

--- lista2/reguaInglesa.c
+++ lista2/reguaInglesa.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-int pot(int a, int b)
-{
-    if(b==0)
-        return 1;
-    else 
-        return a*pot(a,b-1);
-}
 void desenho_tracos(int ordem)
 {
     if(ordem > 0){
@@ -14,24 +7,22 @@ void desenho_tracos(int ordem)
     }else
         printf("\n");
 }
-void regua_inglesa(int ordem, int n_dots)
+void regua_inglesa(int ordem)
 {
-    int pm = n_dots/2;
-    if(ordem >0){
-        regua_inglesa(ordem-1, pm);
-        printf("ORDEM: %d\n", ordem);
+    if(ordem >0)
+    {
+        regua_inglesa(ordem-1);
         printf(".");
         desenho_tracos(ordem);
-        regua_inglesa(ordem-1, pm);
+        regua_inglesa(ordem-1);
     }else
         return;
 
 }
 int main()
 {
-    int ordem, n_dots;
+    int ordem;
     scanf("%d", &ordem);
-    n_dots = pot(2,ordem) -1;
-    regua_inglesa(ordem, n_dots);
+    regua_inglesa(ordem);
     return 0;
 }
